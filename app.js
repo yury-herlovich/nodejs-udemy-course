3a9363665ca900587da67ebfe3670df5fecfea79
Yury Herlovich (Vertecx) <yuryherlovich@gmail.com>
2018-08-14 16:16:59 -0700

    finish first section

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const notes = require("./notes");
 
-var note1 = {};
-
-debugger;
-
 // notes.addNote("title2", "body");
 // notes.removeNote("title");
 // var note = notes.getNote("title2");
+// var allNotes = notes.getAll();
