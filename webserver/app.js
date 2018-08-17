2d953799c2786328197b0d5a5f47f075eb75b312
Yury Herlovich <yuryherlovich@gmail.com>
2018-08-17 16:37:32 -0700

    Added public folder

--- webserver/app.js
+++ webserver/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 var app = express();
 
+app.use(express.static(__dirname + "/public/"))
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
