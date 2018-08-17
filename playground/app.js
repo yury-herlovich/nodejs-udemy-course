2dd3347374c39cf8e8651ce67b7f04984a22b9ac
Yury Herlovich <yuryherlovich@gmail.com>
2018-08-16 21:33:36 -0700

    Rewrote weather with promises

--- playground/app.js
+++ playground/app.js
@@ -1,9 +1,34 @@
-var somePromise = new Promise((resolve, reject) => {
-  resolve("It works");
-});
+// var somePromise = new Promise((resolve, reject) => {
+//   resolve("It works");
+// });
 
-somePromise.then((message) => {
-  console.log("Success: ", message);
-}, (errorMessage) => {
-  console.log("Error: ", errorMessage);
-});
+// somePromise.then((message) => {
+//   console.log("Success: ", message);
+// }, (errorMessage) => {
+//   console.log("Error: ", errorMessage);
+// });
+
+
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === "number" && typeof b === "number") {
+        resolve(a + b);
+      } else {
+        reject("Arguments must be numbers");
+      }
+    }, 1000);
+  });
+};
+
+asyncAdd(5, 7)
+  .then((res) => {
+    console.log("Result: ", res);
+    return asyncAdd(res, 30);
+  })
+  .then((res) => {
+    console.log("Result: ", res);
+  })
+  .catch((errorMessage) => {
+    console.log("Error: ", errorMessage);
+  });
