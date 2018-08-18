b002c3f9f54a55f321592bd1ded6aeee57e6b654
Yury Herlovich <yuryherlovich@gmail.com>
2018-08-17 17:36:32 -0700

    Added template engine

--- webserver/app.js
+++ webserver/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials/");
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public/"))
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+})
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test.hbs", {
+    pageTitle: "Test page"
+  });
 });
 
 app.listen(3000, () => {
