c88eae06ee3a361bf6639967f04856edcd649142
Yury Herlovich <yuryherlovich@gmail.com>
2018-08-19 12:26:01 -0700

    logging

--- webserver/app.js
+++ webserver/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials/");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public/"))
+
+// log
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} - ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('app.log', log + "\n", (err) => {});
+
+  next()
+});
+
 
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
