0ecd65638ea03f8a2498fae483a53b6b231e0776
Yury Herlovich (Vertecx) <yuryherlovich@gmail.com>
2018-08-21 13:43:35 -0700

    Remove mongo image, add mLab mongodb

--- todo_api/playground/mongodb-connect.js
+++ todo_api/playground/mongodb-connect.js
@@ -1,16 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-const mongoURL = 'mongodb://mongo:27017';
-
-// Database Name
-const dbName = 'test';
+const mongoUser = 'qw4u';
+const mongoPass = 'EVSm6pNugKpvEosT';
+const mongoURL = `mongodb://${mongoUser}:${mongoPass}@ds229552.mlab.com:29552/yury-test`;
 
 // Use connect method to connect to the server
-MongoClient.connect(mongoURL, function(err, client) {
+MongoClient.connect(mongoURL, { useNewUrlParser: true }, function(err, db) {
   console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
-
-  client.close();
+  db.close();
 });
