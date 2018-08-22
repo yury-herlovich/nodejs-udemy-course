053159a5779bdeb5eb7327e3e9818e69e74134c5
Yury Herlovich <yuryherlovich@gmail.com>
2018-08-21 19:42:14 -0700

    Add delete script

--- todo_api/app.js
+++ todo_api/app.js
@@ -1,2 +1,3 @@
 require('dotenv').config();
-require('./playground/mongodb-find');
+require('./playground/mongodb-connect');
+require('./playground/mongodb-delete');
--- todo_api/playground/mongodb-delete.js
+++ todo_api/playground/mongodb-delete.js
@@ -14,16 +14,23 @@ MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, client) => {
   console.log("Connected to MongoDB server");
   var db=client.db(envConfig.MONGO_DB);
 
-  db.collection('Todos').insertOne({
-    text: 'Something to do',
-    completed: false
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert todo', err);
-    }
+  // db.collection('Todos')
+  //   .deleteMany({text: 'Something to do'})
+  //   .then((res) => {
+  //     console.log(res.deletedCount);
+  //   });
 
-    console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), null, 2));
-  });
+  // db.collection('Todos')
+  //   .deleteOne({text: 'Something to do'})
+  //   .then((res) => {
+  //     console.log(res.deletedCount);
+  //   });
+
+  db.collection('Todos')
+    .findOneAndDelete({text: 'Something to do'})
+    .then((res) => {
+      console.log(res.value);
+    });
 
   client.close();
 });
