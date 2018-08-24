e4cbb359c8dd21a13157c64b2894621d2b744f46
Yury Herlovich <yuryherlovich@gmail.com>
2018-08-24 13:00:26 -0700

    Add POST /todos route

--- todo_api/app.js
+++ todo_api/app.js
@@ -1,4 +1,30 @@
 require('dotenv').config();
-var { mongoose } = require('./db/mongoose');
-var { User } = require('./models/user');
-var { Todo } = require('./models/todo');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
+
+var app = express();
+
+// add body-parser middleware
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+     text: req.body.text
+  });
+
+  todo.save()
+    .then((doc) => {
+      res.status(201).send(doc);
+    }, (err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000.');
+});
