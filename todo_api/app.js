2caca2a5efb4bbf3d6ea14fa3bf55175c7d076bf
Yury Herlovich (Vertecx) <yuryherlovich@gmail.com>
2018-08-27 17:13:33 -0700

    Add PATCH /todos/:id route

--- todo_api/app.js
+++ todo_api/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const _ = require('lodash');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -69,6 +70,36 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    })
+    .catch((err) => {
+      res.status(400).send();
+    });
+});
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
     .then((todo) => {
       if (!todo) {
         return res.status(404).send();
