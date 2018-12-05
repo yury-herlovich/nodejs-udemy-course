126b7d15785955e94107963e6abb9431095b826f
Yury Herlovich <Yury.Herlovich@motortrend.com>
2018-12-04 16:17:03 -0800

    Chat: adds new docker conf, socket.io to the project

---
+++ chat/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, 'public');
+const port = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user connected!');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected.');
+    });
+});
+
+server.listen(port, () => console.log(`App listens on port ${port}.`));
